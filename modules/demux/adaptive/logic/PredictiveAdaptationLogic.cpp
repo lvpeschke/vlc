4197fb8ca6625e2b80071aeff2f887a03fcb9bcf
lvpeschke <lena.peschke@student.uclouvain.be>
2016-10-06 17:06:28 +0200

    Correct format error for predictive logic log messages

--- modules/demux/adaptive/logic/PredictiveAdaptationLogic.cpp
+++ modules/demux/adaptive/logic/PredictiveAdaptationLogic.cpp
@@ -100,11 +100,11 @@ BaseRepresentation *PredictiveAdaptationLogic::getNextRepresentation(BaseAdaptat
             }
         }
         /* LVP added, TFE */
-        std::cerr << "TFE predictive buflevel" << mdate() << ", " << f_buffering_level << std::endl;
-        std::cerr << "TFE predictive stats buflevel" << mdate() << ", " << stats.buffering_level << std::endl;
-        std::cerr << "TFE predictive stats buftarget" << mdate() << ", " << stats.buffering_target << std::endl;
-        std::cerr << "TFE predictive min buflevel" << mdate() << ", " << f_min_buffering_level << std::endl;
-        std::cerr << "TFE predictive max bitrate" << mdate() << ", " << i_max_bitrate << std::endl;
+        std::cerr << "TFE predictive buflevel, " << mdate() << ", " << f_buffering_level << std::endl;
+        std::cerr << "TFE predictive stats buflevel, " << mdate() << ", " << stats.buffering_level << std::endl;
+        std::cerr << "TFE predictive stats buftarget, " << mdate() << ", " << stats.buffering_target << std::endl;
+        std::cerr << "TFE predictive min buflevel, " << mdate() << ", " << f_min_buffering_level << std::endl;
+        std::cerr << "TFE predictive max bitrate, " << mdate() << ", " << i_max_bitrate << std::endl;
 
         if(stats.starting())
         {
